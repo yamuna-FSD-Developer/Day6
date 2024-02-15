7c73251296e5ae3f13acd1817686e4b161a7a38d
Yamuna PG <150881590+yamuna-FSD-Developer@users.noreply.github.com>
2024-02-15 21:01:51 +0530

    Update c2.js

--- c2.js
+++ c2.js
@@ -1,13 +1,15 @@
-class Uber{
-    constructor(baseFare,costPerKm,costPerMinute){
-        this.baseFare=baseFare;
-        this.costPerKm=costPerKm;
-        this.costPerMinute=costPerMinute;
+class Uber {
+    constructor(baseFare, costPerKm, costPerMinute) {
+        this.baseFare = parseFloat(baseFare); 
+        this.costPerKm = parseFloat(costPerKm); 
+        this.costPerMinute = parseFloat(costPerMinute); 
     }
-    getuber(){
-        return `Totalprice is ${this.baseFare + this.costPerKm + this.costPerMinute} `;
+
+    getUber(distanceKm, timeMinutes) {
+        const totalPrice = this.baseFare + (distanceKm * this.costPerKm) + (timeMinutes * this.costPerMinute);
+        return `Total price is ${totalPrice.toFixed(2)}`;
     }
 }
 
-var u1 = new Uber("25","18","9");
-console.log(u1.getuber());
+const u1 = new Uber("25", "18", "9");
+console.log(u1.getUber(10, 20));
