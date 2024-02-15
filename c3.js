3d4c0f0b2623d15dc730c72980fd37ac2e1deb09
Yamuna PG <150881590+yamuna-FSD-Developer@users.noreply.github.com>
2024-02-15 21:03:28 +0530

    Update c3.js

--- c3.js
+++ c3.js
@@ -1,13 +1,12 @@
-class Movie{
-    2	    constructor(title,studio,rating){
-    3	        this.title=title;
-    4	        this.studio=studio;
-    5	        this.rating=rating;
-    6	    }
-    7	    getmovie(){
-    8	        return `The Title of Movie is ${this.title}, and production studio ${this.studio}, and rating is ${this.rating}. `;
-    9	
-    10	    }
-    11	}
-    12	var m1 = new Movie("OKK","MR","4.5");
-    13	console.log(m1.getmovie());
+class Movie {
+      constructor(title,studio,rating){
+    	        this.title=title;
+    	        this.studio=studio;
+    	        this.rating =rating;
+    	    }
+    	    getmovie(){
+    	        return `The Title of Movie is ${this.title}, and production studio ${this.studio}, and rating is PG ${this.rating}. `;
+    	
+    	    }
+    	}
+    	var m1 = new Movie("OKK","MR","4.5");	console.log(m1.getmovie());
